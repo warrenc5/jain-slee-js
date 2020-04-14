cc46954f4a64e37f13dc0b339454ec5795c06a81
warrenc5 <warren@mofokom.biz>
2020-04-14 18:01:43 +1000

    add test for native-image

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-JAVA_HOME=/usr/local/java/graalvm-ce-java11-20.0.0/ mvn -Pgraal install 
+JAVA_HOME=/usr/local/java/graalvm-ce-java11/ mvn -Pgraal clean install 
 cp target/jslee-js bin
-
+#--add-exports=java.base/jdk.internal.module=ALL-UNNAMED
 bin/jslee-js --js.username=wozza --js.password=wozza --js.url=service:jmx:remote+http://localhost:9990 src/test/js/service-activate-deactivate.js 
